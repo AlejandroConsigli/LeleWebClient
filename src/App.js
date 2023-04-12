d1021d01fa1c6ca3126cee4454c97c628f6e1285
Alejandro <consiglialejandro@gmail.com>
2023-04-12 12:31:59 +0200

    Uploading first files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [data, setData] = useState("Connecting...");
+
+  useEffect(() => {
+    fetch("https://lele-web-server.vercel.app")
+      .then((res) => res.json())
+      .then(({ message }) => setData(message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Welcome to Lele Web</h1>
+      <span className="subtitle">{data}</span>
     </div>
   );
 }

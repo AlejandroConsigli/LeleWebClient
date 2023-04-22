c74d9b6d196f8e8c67e578a991876b73820ae980
Alejandro <consiglialejandro@gmail.com>
2023-04-22 17:44:15 +0200

    New favicon and url server

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   let [data, setData] = useState("Connecting...");
 
   useEffect(() => {
-    fetch("https://lele-web-server.vercel.app")
+    fetch("https://lele-web-server.vercel.app/api")
       .then((res) => res.json())
       .then(({ message }) => setData(message));
   }, []);
 
   return (
     <div className="container">
-      <h1 className="title">Welcome to Lele Web</h1>
+      <h1 className="title">Welcome to Lele Web Greetings</h1>
       <span className="subtitle">{data}</span>
     </div>
   );
